0212743d355036d79b38b579c8df66d6f2366f0c
Stephen Alvarez <Stephenalvarez3@gmail.com>
2024-11-06 15:17:03 +0000

    Query to insert account into account db

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -6,5 +6,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AccountDAO {
-    
+
+
+    /*
+     * @param account: id is not needed
+     * @return: account if insertion was successful, return null if not successful.
+     */
+    public Account insertAccount(Account account){
+        /*
+         * Account 
+         *  id
+         *  username
+         *  password
+         */
+
+        Connection connection = ConnectionUtil.getConnection();
+
+        try{
+            String sql = "INSERT INTO account (username, password) VALUES (?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, account.getUsername());
+            preparedStatement.setString(1, account.getPassword());
+
+            preparedStatement.executeUpdate();
+            return account;
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
 }
